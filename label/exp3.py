97f7460a1f84ee369308e884174a50bdfaf100cd
poteman <946691288@qq.com>
2019-08-17 22:01:17 +0800

    实验3,10折融合,每次都带5,且不训练第二次,带有验证集以获得较好的迭代轮次

--- label/exp3.py
+++ label/exp3.py
@@ -1031,67 +1031,77 @@ def time_kfold(length, idx, kfold=5):
 
     nround = False
     if idx == 1:
-        local_train_idx = idxs["idx4"]
-        local_valid_idx = idxs["idx5"]
-        train_idx = idxs["idx5"]
+        local_train_idx = idxs["idx2"] + idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
+        local_valid_idx = idxs["idx1"]
+        train_idx = local_train_idx
         train_first = True
-        train_second = True
+        train_second = False
     if idx == 2:
-        local_train_idx = idxs["idx3"] + idxs["idx4"]
-        local_valid_idx = idxs["idx5"]
-        train_idx = idxs["idx4"] + idxs["idx5"]
-        train_first = True
-        train_second = True
-    if idx == 3:
-        local_train_idx = idxs["idx2"] + idxs["idx3"] + idxs["idx4"]
-        local_valid_idx = idxs["idx5"]
-        train_idx = idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
-        train_first = True
-        train_second = True
-    if idx == 4:
-        local_train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx3"] + idxs["idx4"]
-        local_valid_idx = idxs["idx5"]
-        train_idx = idxs["idx2"] + idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
-        train_first = True
-        train_second = True
-    if idx == 5:
-        local_train_idx = None
-        local_valid_idx = None
-        train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
-        train_first = False
-        train_second = True
-        nround = True
-    if idx == 6:
         local_train_idx = idxs["idx1"] + idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
         local_valid_idx = idxs["idx2"]
-        train_idx = idxs["idx1"] + idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
+        train_idx = local_train_idx
+        train_first = True
+        train_second = False
+    if idx == 3:
+        local_train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx4"] + idxs["idx5"]
+        local_valid_idx = idxs["idx3"]
+        train_idx = local_train_idx
+        train_first = True
+        train_second = False
+    if idx == 4:
+        local_train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx3"] + idxs["idx5"]
+        local_valid_idx = idxs["idx4"]
+        train_idx = local_train_idx
+        train_first = True
+        train_second = False
+    if idx == 5:
+        local_train_idx = idxs["idx3"] + idxs["idx4"] + idxs["idx5"]
+        local_valid_idx = idxs["idx1"] + idxs["idx2"]
+        train_idx = local_train_idx
+        train_first = True
+        train_second = False
+    if idx == 6:
+        local_train_idx = idxs["idx2"] + idxs["idx4"] + idxs["idx5"]
+        local_valid_idx = idxs["idx1"] + idxs["idx3"]
+        train_idx = local_train_idx
         train_first = True
         train_second = False
     if idx == 7:
-        local_train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx4"] + idxs["idx5"]
-        local_valid_idx = idxs["idx3"]
-        train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx4"] + idxs["idx5"]
+        local_train_idx = idxs["idx2"] + idxs["idx3"] + idxs["idx5"]
+        local_valid_idx = idxs["idx1"] + idxs["idx4"]
+        train_idx = local_train_idx
         train_first = True
         train_second = False
     if idx == 8:
-        local_train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx3"] + idxs["idx5"]
-        local_valid_idx = idxs["idx4"]
-        train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx3"] + idxs["idx5"]
+        local_train_idx = idxs["idx1"] + idxs["idx4"] + idxs["idx5"]
+        local_valid_idx = idxs["idx2"] + idxs["idx3"]
+        train_idx = local_train_idx
+        train_first = True
+        train_second = False
+    if idx == 9:
+        local_train_idx = idxs["idx1"] + idxs["idx3"] + idxs["idx5"]
+        local_valid_idx = idxs["idx2"] + idxs["idx4"]
+        train_idx = local_train_idx
+        train_first = True
+        train_second = False
+    if idx == 10:
+        local_train_idx = idxs["idx1"] + idxs["idx2"] + idxs["idx5"]
+        local_valid_idx = idxs["idx3"] + idxs["idx4"]
+        train_idx = local_train_idx
         train_first = True
         train_second = False
 
     return local_train_idx, local_valid_idx, train_idx, train_first, train_second, nround
-
 
 lgb_sub = sub.copy()
 lgb_sub['isFraud'] = 0
 aucs = []
 best_iters = []
 
-n_fold = 8
+n_fold = 10
 training_start_time = time()
 length = len(X)
-for idx in range(1, 9):
+for idx in range(1, n_fold+1):
     start_time = time()
     print('Training on fold {}'.format(idx))
 
