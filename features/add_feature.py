2355dbb0d1f239fe5df49db3ed59137f7bee173d
poteman <946691288@qq.com>
2019-08-20 19:19:59 +0800

    增加D10和D15特征,DAY取消120的限制

--- features/add_feature.py
+++ features/add_feature.py
@@ -1031,9 +1031,15 @@ else:
     # test_X = pd.concat([test_X.drop("card5", axis=1), card5_onehot_test.reset_index()], axis=1)
 
     # 增加uid_D15特征
-    uid_D15_train = pd.read_csv("./uid_D15_train_modify.csv")
+    uid_D15_train = pd.read_csv("./train_target_encoding_D15.csv")
     X = X.merge(uid_D15_train, on="TransactionID", how="left")
     test_X = test_X.merge(uid_D15_train, on="TransactionID", how="left")
+
+    # 增加uid_D10特征
+    uid_D10_train = pd.read_csv("./train_target_encoding_D10.csv")
+    X = X.merge(uid_D10_train, on="TransactionID", how="left")
+    test_X = test_X.merge(uid_D10_train, on="TransactionID", how="left")
+
 
 X = X.drop('TransactionID', axis=1)
 test_X = test_X.drop('TransactionID', axis=1)
